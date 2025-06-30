fb90fd0c4b3b3b4dc52366bd8bdfa2ebee1e73eb
DriftingStarry <doubichenzi@126.com>
2025-06-30 16:20:48 +0800

    chore(frontend): 修改页脚信息

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,7 +32,7 @@
 
       <!-- 底部 -->
       <el-footer class="app-footer">
-        <p>&copy; 2024 图书推荐系统 - 基于 goodbooks-10k 数据集</p>
+        <p>&copy; 图书推荐系统 - 基于 goodbooks-10k 数据集</p>
       </el-footer>
     </el-container>
   </div>
